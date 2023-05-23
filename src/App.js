ade5fad3e380c4379dd05190f9dce21b7c3f201b
Yash <yashrajoriya321@gmail.com>
2023-05-23 16:12:06 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Home from "./pages/Home";
 import Event from "./pages/Event";
@@ -10,17 +10,19 @@ import Blogs from "./pages/Blog";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="event" element={<Event />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="aboutus" element={<AboutUs />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="event" element={<Event />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="aboutus" element={<AboutUs />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
